001c84404e480bae5c91ef7210db5635ba7a5903
TNoga0 <t.noga0@gmail.com>
2020-09-13 15:30:36 +0200

    Updates

---
+++ precache-manifest.d83bac56b124d7448aea508b11063ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d93b863cd6ee0d665a9d0f6d63ec1e",
+    "url": "/chartjs-throws/index.html"
+  },
+  {
+    "revision": "e2d3723c22c4085bc8df",
+    "url": "/chartjs-throws/static/css/main.51632ed9.chunk.css"
+  },
+  {
+    "revision": "85ee3688c2d03ff8002a",
+    "url": "/chartjs-throws/static/js/2.478ad7b1.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/chartjs-throws/static/js/2.478ad7b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d3723c22c4085bc8df",
+    "url": "/chartjs-throws/static/js/main.c02726c5.chunk.js"
+  },
+  {
+    "revision": "cedb52831200f0430942",
+    "url": "/chartjs-throws/static/js/runtime-main.3ff70634.js"
+  }
+]);
